58a4fe995d8e2c76b0af846213ceed59e175d680
essential-workshop-9972 <josephjones7@gmail.com>
2020-01-03 15:49:12 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,11 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  puts find_element_index.find_index()
 end
 
-def find_max_value(array)
-  # Add your solution here
+def find_max_value([1,2,1,3,4,3,5,4,3,2,1])
+  puts find_max_value.max
 end
 
-def find_min_value(array)
-  # Add your solution here
+def find_min_value([10,5,3,7,19,1,3,10])
+  puts find_min_value.min 
 end
